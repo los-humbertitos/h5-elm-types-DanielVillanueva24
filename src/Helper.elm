1a497e65a7ea772c0fd59b33d0191c0cc8f0d70d
DanielVillenueva24 <up230591@alumnos.upa.edu.mx>
2026-03-01 21:46:27 -0600

    Tarea 5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,106 @@
-module Helper exposing (..)
+module Main exposing
+    ( AirplaneStatus(..)
+    , GradeStatus(..)
+    , airplaneScheduleAction
+    , airportAction
+    , categoricalGrade
+    , main
+    )
+
+import Browser
+import Html exposing (Html, li, text, ul)
+
+
+type GradeStatus
+    = Approved
+    | Failed
+    | Pending
+
+
+categoricalGrade : List Float -> List GradeStatus
+categoricalGrade grades =
+    List.map gradeToStatus grades
+
+
+gradeToStatus : Float -> GradeStatus
+gradeToStatus grade =
+    if grade < 0 then
+        Pending
+
+    else if grade > 7 then
+        Approved
+
+    else
+        Failed
+
+
+type AirplaneStatus
+    = OnTime
+    | Boarding
+    | Delayed
+    | Cancelled
+
+
+airplaneScheduleAction : AirplaneStatus -> String
+airplaneScheduleAction status =
+    case status of
+        Cancelled ->
+            "Pedir reembolso"
+
+        Delayed ->
+            "Esperar"
+
+        OnTime ->
+            "Esperar"
+
+        Boarding ->
+            "Buscar boleto"
+
+
+airportAction : List AirplaneStatus -> List String
+airportAction statuses =
+    List.map airplaneScheduleAction statuses
+
+
+type alias Model =
+    { grades : List Float
+    , flights : List AirplaneStatus
+    }
+
+
+init : Model
+init =
+    { grades = [ 10, 7, 6.9, -1, 8.5 ]
+    , flights = [ Cancelled, Delayed, OnTime, Boarding ]
+    }
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> Model
+update msg model =
+    model
+
+
+view : Model -> Html Msg
+view model =
+    ul []
+        (List.map liText
+            (airportAction model.flights)
+        )
+
+
+liText : String -> Html Msg
+liText s =
+    li [] [ text s ]
+
+
+main : Program () Model Msg
+main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
