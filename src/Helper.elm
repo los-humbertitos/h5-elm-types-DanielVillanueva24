b66b8c4228933054cd56f20e1455e2ffddb7388a
DanielVillenueva24 <up230591@alumnos.upa.edu.mx>
2026-03-01 21:52:48 -0600

    tarea 5

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,14 +1,4 @@
-module Helper exposing
-    ( AirplaneStatus(..)
-    , GradeStatus(..)
-    , airplaneScheduleAction
-    , airportAction
-    , categoricalGrade
-    , main
-    )
-
-import Browser
-import Html exposing (Html, li, text, ul)
+module Helper exposing (AirplaneStatus(..), GradeStatus(..), airplaneScheduleAction, airportAction, categoricalGrade)
 
 
 type GradeStatus
@@ -27,7 +17,7 @@ gradeToStatus grade =
     if grade < 0 then
         Pending
 
-    else if grade > 7 then
+    else if grade >= 7 then
         Approved
 
     else
@@ -60,47 +50,3 @@ airplaneScheduleAction status =
 airportAction : List AirplaneStatus -> List String
 airportAction statuses =
     List.map airplaneScheduleAction statuses
-
-
-type alias Model =
-    { grades : List Float
-    , flights : List AirplaneStatus
-    }
-
-
-init : Model
-init =
-    { grades = [ 10, 7, 6.9, -1, 8.5 ]
-    , flights = [ Cancelled, Delayed, OnTime, Boarding ]
-    }
-
-
-type Msg
-    = NoOp
-
-
-update : Msg -> Model -> Model
-update msg model =
-    model
-
-
-view : Model -> Html Msg
-view model =
-    ul []
-        (List.map liText
-            (airportAction model.flights)
-        )
-
-
-liText : String -> Html Msg
-liText s =
-    li [] [ text s ]
-
-
-main : Program () Model Msg
-main =
-    Browser.sandbox
-        { init = init
-        , update = update
-        , view = view
-        }
